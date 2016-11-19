543be2f8fa606974f0c2df2df187de354546dc2a
Unknown <pawel.trysla@gmail.com>
2016-11-19 15:04:32 +0100

    feat(RestClient, App): initial implementation.

---
+++ src/rest-client/RestClient.js
@@ -0,0 +1,35 @@
+export class RestClient {
+	constructor() {
+	}
+
+	getPosts() {
+		const identityPromises = [];
+		let id = 0;
+
+		for(let i = 0; i < 120; i++) {
+			identityPromises.push(
+				fetch('http://uinames.com/api/?ext')
+				.then((response) => {
+					return response.json();
+				})
+			);
+		}
+
+
+		return Promise.all(identityPromises)
+		.then((identities) => {
+			return Promise.resolve(
+				identities.map((identity) => {
+					return {
+						id: id++,
+						username: `${identity.name} ${identity.surname}`,
+						postTitle: `I say: ${identity.region} is awesome!`,
+						views: identity.age,
+						likes: identity.age * 2,
+						createdAt: new Date(identity.birthday.mdy)
+					}
+				})
+			);
+		});
+	}
+}
